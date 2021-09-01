2fbed143dff210dc5012dc3781c24bb9488c81c4
Melanie Sarrouy <sarsarmel@gmail.com>
2021-09-01 17:06:53 +0200

    initialisation du projet

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import logo from '../assets/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Initialisation appli Kasa avec CRA
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
